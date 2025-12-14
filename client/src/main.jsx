38ffa8fb885eae3b72113a633d0bb5f2d770f315
refentse <refentse.ledwaba@gmail.com>
2025-12-14 13:13:23 +0200

    test: intentional failure drill

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+throw new Error("CI failure drill");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
