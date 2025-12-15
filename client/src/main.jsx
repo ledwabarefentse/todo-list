0eedbfc9635598d58c478bac772e8498a7858a87
refentse <refentse.ledwaba@gmail.com>
2025-12-15 09:55:22 +0200

    Revert "test: intentional failure drill"
    This reverts commit 38ffa8fb885eae3b72113a633d0bb5f2d770f315.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-throw new Error("CI failure drill");
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
